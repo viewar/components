a964d8622086ab15702d5909ddfe4eb7dc54f12a
DoubleU23 <5007050+DoubleU23@users.noreply.github.com>
2019-11-16 17:43:46 +0100

    feat(viewar): add 'viewar-core' and 'viewar-api' (#12)
    * add '.viewar-config'
    * update index.html to include core + core vars
    * move App to own file
    * deps: add 'viewar-api' and 'viewar-core'
    * expose `window.api`

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
+
 const rootElement = document.getElementById('app-root') || document.getElementById('app');
 const render = (Component) => {
   // eslint-disable-next-line react/jsx-filename-extension
   ReactDOM.render(<Component />, rootElement);
 };
-
-const App = () => (
-  <div id="app_wrapper">
-    <h1>@viewar/components</h1>
-  </div>
-);
 
 render(App);
 

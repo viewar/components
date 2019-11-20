fff1af6361be260bbcccf5188139f644069df2c8
DoubleU23 <5007050+DoubleU23@users.noreply.github.com>
2019-11-20 20:34:23 +0100

    feat(componentPresenter): render Showcases per router param (#20)
    * add component exports to '/components/index.js'
    * chore(deps): add 'react-router-dom'
    * add showcase exports to '/components/showcases'
    * add 'ComponentPresenter'
    * render 'ComponentPresenter'  with route param 'componentName'
    * feat(husky): add `npm test` to 'pre-push' hooks
    * fix lint: add propType for `props.match.paras.componentName`

---
+++ src/ComponentPresenter/ComponentPresenter.jsx
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+import * as showcases from 'components/showcases';
+
+class ComponentPresenter extends PureComponent {
+    static propTypes = {
+      match: PropTypes.shape({
+        params: PropTypes.shape({
+          componentName: PropTypes.string,
+        }).isRequired,
+      }).isRequired,
+    }
+
+    render() {
+      const { match: { params: { componentName }}} = this.props;
+
+      // eslint-disable-next-line import/namespace
+      const ShowcaseComponent = showcases[componentName];
+
+      return (
+        <div id="ComponentPresenter" key="ComponentPresenter">
+          <h3>Showcase for {`<${componentName} />`}</h3>
+          {!!ShowcaseComponent && <ShowcaseComponent /> || <div>Showcase not defined.</div>}
+        </div>
+      );
+    }
+}
+
+export default ComponentPresenter;

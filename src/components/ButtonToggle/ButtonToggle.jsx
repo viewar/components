bc552bc4ad6d566f37dafd09456c4945c66fdcca
SF <sf@viewar.com>
2019-11-20 13:30:16 +0100

    fix(ButtonToggle): allow `<ButtonToggle />` to be un-controlled

--- src/components/ButtonToggle/ButtonToggle.jsx
+++ src/components/ButtonToggle/ButtonToggle.jsx
@@ -1,34 +1,70 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import styles from './ButtonToggle.scss';
 
-const ButtonToggle = ({ label, checked, onToggle }) => (
-  <div styles={styles.ButtonToggle} onClick={onToggle}>
-    <label className={styles.switch}>
-      <input
-        id="animationName0"
-        type="checkbox"
-        checked={checked}
-        onChange={onToggle}
-      />
-      <span className={styles.slider + ' ' + styles.round} />
-    </label>
-    <label className={styles.ToggleName}>
-      {label}
-    </label>
-  </div>
-);
+class ButtonToggle extends PureComponent {
+  static propTypes = {
+    label:        PropTypes.string.isRequired,
+    checked:      PropTypes.bool,
+    onChange:     PropTypes.func,
+    initialState: PropTypes.shape({
+      checked: PropTypes.bool.isRequired,
+    }),
+  };
 
-ButtonToggle.propTypes = {
-  label:    PropTypes.string.isRequired,
-  checked:  PropTypes.bool,
-  onToggle: PropTypes.func,
-};
+  static defaultProps= {
+    checked:      null,
+    onChange:     () => {},
+    initialState: { checked: true },
+  };
 
-ButtonToggle.defaultProps = {
-  checked:  false,
-  onToggle: () => {},
-};
+  constructor(props) {
+    super(props);
+    const { checked, initialState } = props;
+
+    this.isControlled = checked != null;
+
+    if (!this.isControlled) {
+      this.state = initialState;
+    }
+  }
+
+  handleOnChange = ({ currentTarget: { checked }}) => {
+    if (!this.isControlled) {
+      this.setState({ checked });
+    }
+
+    this.props.onChange(checked);
+  }
+
+  render() {
+    const { label } = this.props;
+
+    const checked = this.isControlled
+      ? this.props.checked
+      : this.state.checked
+    ;
+
+    return (
+      <>
+        <div styles={styles.ButtonToggle}>
+          <label className={styles.switch}>
+            <input
+              id="animationName0"
+              type="checkbox"
+              checked={checked}
+              onChange={this.handleOnChange}
+            />
+            <span className={styles.slider + ' ' + styles.round} />
+          </label>
+          <label className={styles.ToggleName}>
+            {label}
+          </label>
+        </div>
+      </>
+    );
+  }
+}
 
 export default ButtonToggle;

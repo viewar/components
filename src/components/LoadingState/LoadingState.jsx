1f8e3e03d9ea677ca3ab3a8188ea15cef1ad1f02
SF <sf@viewar.com>
2019-12-09 16:06:11 +0100

    comp(LoadingState): add props.isOverlay (default: true)

--- src/components/LoadingState/LoadingState.jsx
+++ src/components/LoadingState/LoadingState.jsx
@@ -2,22 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-// TODO: copy assets on build or fix file-loader
 // this import triggers 'file-loader' to copy assets into /build of 'webpack-dev-server'
 import LoadingGear from 'assets/viewar_loading_gear_red.svg'; // eslint-disable-line no-unused-vars
+import Icon from 'components/Icon';
 
 import styles from './LoadingState.scss';
 
 
-const LoadingState = ({ isVisible }) => {
+const LoadingState = ({ isVisible, isOverlay }) => {
   return (
     <div className={cx(styles.LoadingState, {
-      [styles.isHidden]: !isVisible,
+      [styles.isHidden]:  !isVisible,
+      [styles.isOverlay]: isOverlay,
     })}
     >
-      <div className={styles.img}>
-        {/* LOADING IMAGE addd by CSS background */}
-      </div>
+      <Icon icon="refresh" className={styles.img} />
       <div className={styles.text}>Loading ...</div>
     </div>
   );
@@ -25,10 +24,12 @@ const LoadingState = ({ isVisible }) => {
 
 LoadingState.propTypes = {
   isVisible: PropTypes.bool,
+  isOverlay: PropTypes.bool,
 };
 
 LoadingState.defaultProps = {
   isVisible: false,
+  isOverlay: true,
 };
 
 export default LoadingState;

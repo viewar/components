4d6e4819eebe0715fd252a8363864d764444cfc0
viewar_sf <dev@project23.org>
2019-12-20 17:10:28 +0100

    chore(build): remove && chaining of 'rm'

--- build_package.sh
+++ build_package.sh
@@ -7,9 +7,11 @@ rm ./dist -rf
 ./node_modules/.bin/babel ./src/components -d dist/components --copy-files --compact --minified
 
 # * copy /src/sass to /dist/sass
-rm ./dist/sass -rf && mkdir ./dist/sass
+rm ./dist/sass -rf
+mkdir ./dist/sass
 cp ./src/sass/* ./dist/sass -r
 
 # * copy /src/assets to /dist/assets
-rm ./dist/assets -rf && mkdir ./dist/assets
+rm ./dist/assets -rf
+mkdir ./dist/assets
 cp ./src/assets/* ./dist/assets -r

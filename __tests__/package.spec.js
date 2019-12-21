e8fd48088444294428f6840b88a7e40d04fc25c4
viewar_sf <dev@project23.org>
2019-12-22 00:03:16 +0100

    feat(unit-tests): add jest unit tests for npm scripts

---
+++ __tests__/package.spec.js
@@ -0,0 +1,26 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+let output = {};
+beforeEach(async () => {
+  if (output.stdout) return output;
+
+  output = await exec('npm run build:package');
+  return output;
+});
+
+test('[clean] should clean dist and build', async () => {
+  const { stderr } = await exec('npm run clean');
+
+  expect(!!stderr).toEqual(false);
+});
+
+test('[build:package] should build dist without error', () => {
+  expect(!!output.stderr).toEqual(false);
+});
+
+test('[build:package] should output \'Successfully compiled\'', () => {
+  const didCompile = (/(successfully) compiled/i).test(output.stdout);
+  expect(didCompile).toEqual(true);
+});
+

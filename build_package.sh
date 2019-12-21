34e362b5cd96369b472167c1bf34a6e65d4b77e6
viewar_sf <dev@project23.org>
2019-12-21 06:07:54 +0100

    chore(build): enhance build script

--- build_package.sh
+++ build_package.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-npm run clean
-
 # * compile components
-npx babel ./src/components -d dist/components --source-maps inline --copy-files --compact --minified
+cross-env NODE_ENV=production babel ./src/components -d dist/components --source-maps inline --copy-files --compact --minified && \
 
 # * copy /src/sass to /dist/sass
-mkdir ./dist/sass
-cp ./src/sass/* ./dist/sass -r
+mkdir ./dist/sass && cp ./src/sass/* ./dist/sass -r
 
-# * copy /src/assets to /dist/assets
-mkdir ./dist/assets
-cp ./src/assets/* ./dist/assets -r
+# # * copy /src/assets to /dist/assets
+mkdir ./dist/assets && cp ./src/assets/* ./dist/assets -r

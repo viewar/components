7cb415f1aa624ae8698b8b259a26bd2374ca468a
viewar_sf <dev@project23.org>
2020-02-25 13:52:16 +0100

    docs(jsdoc-betterdocs): failed on windows build

--- docs/entry.js
+++ docs/entry.js
@@ -1,27 +1,29 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import React from "react";
+
+      import ReactDOM from "react-dom";
 
 
-import ReactWrapper from '../node_modules/better-docs/lib/react-wrapper.js';
+      import ReactWrapper from '../node_modules/better-docs/lib/react-wrapper.js';
 
+      window.React = React;
 
-import './styles/reset.css';
+      window.ReactDOM = ReactDOM;
 
-import './styles/iframe.css';
+      window.ReactWrapper = ReactWrapper;
 
-import Component0 from '../dist/components/Overlay/Overlay.js';
-import Component1 from '../dist/components/LoadingState/LoadingState.js';
+    
+    import './styles/reset.css';
 
+    import './styles/iframe.css';
 
-window.reactComponents = {};
-
-window.vueComponents = {};
-
-window.React = React;
-
-window.ReactDOM = ReactDOM;
-
-window.ReactWrapper = ReactWrapper;
+  import Component0 from '../dist/components/Overlay/Overlay.js';
 reactComponents['Overlay'] = Component0;
+
+import Component1 from '../dist/components/LoadingState/LoadingState.js';
 reactComponents['LoadingState'] = Component1;

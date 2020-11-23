2fdc6288e88bdc2ab8ebc262a837f4b16ca41cca
Karl Hofer <kh@viewar.com>
2020-11-23 14:04:30 +0100

    Fix build script (asset copying)

--- build_package.sh
+++ build_package.sh
@@ -4,7 +4,7 @@
 npx cross-env NODE_ENV=production babel ./src/components -d dist/components --source-maps inline --copy-files --compact --minified
 
 # * copy /src/sass to /dist/sass
-mkdir ./dist/sass && cp -R ./src/sass ./dist/sass
+mkdir ./dist/sass && cp -R ./src/sass/ ./dist/sass
 
 # * copy /src/assets to /dist/assets
-mkdir ./dist/assets && cp -R ./src/assets ./dist/assets
+mkdir ./dist/assets && cp -R ./src/assets/ ./dist/assets
